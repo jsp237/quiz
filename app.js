9a62eee797ccc329dc19eafd222df86001d45a50
JERÓNIMO SEGURA POVEDA <jsp237@yahoo.es>
2015-06-21 20:03:54 +0200

    Marco de la Aplicación

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var partials = require('express-partials');
 var routes = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -14,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
+app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
